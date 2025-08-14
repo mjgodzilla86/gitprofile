7f9ab13e585f9bee281fcb133b5b7fc7afb6873f
mjgodzilla86 <mustafa.jung@gmail.com>
2025-08-14 20:15:12 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -79,6 +79,7 @@ const CONFIG = {
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
+    'HTML',
     'PHP',
     'Laravel',
     'JavaScript',
